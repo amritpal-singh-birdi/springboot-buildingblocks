4981ec3df2d5530f9b4528a2b476bd7e065f95ba
Amritpal Singh <amritpalsingh@Mac.galaxus.box>
2025-11-16 02:25:55 +0100

    HelloWorldBase

---
+++ src/main/java/com/stacksimplify/restservices/Hello/HelloWorldController.java
@@ -0,0 +1,29 @@
+package com.stacksimplify.restservices.Hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//Controller
+@RestController
+public class HelloWorldController {
+	
+	//Simple Method
+	//URI - /helloworld
+	//GET
+	/*
+	 * 2 ways
+	 * @RequestMapping
+	 * @GetMapping
+	 * */
+	//@RequestMapping(method = RequestMethod.GET,path = "/helloworld")
+	@GetMapping("/helloworld")
+	public String helloWorld() {
+		return "Hello World";
+	}
+	
+	@GetMapping("/helloworld-bean")
+	public UserDetails helloWorldBean() {
+		return new UserDetails("Teenu", "Diwra", "Indore");
+	}
+
+}
